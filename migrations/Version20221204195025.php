c3c3592232090b148a8c76a4d3e638cb846f6e65
Alex Eremeev <aeremeev@ourgapps.com>
2022-12-04 23:50:53 +0400

    fix migration

--- migrations/Version20221204195025.php
+++ migrations/Version20221204195025.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221201185811 extends AbstractMigration
+final class Version20221204195025 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20221201185811 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE serials ADD last_series INT NOT NULL');
+        $this->addSql('CREATE TABLE serials (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, link VARCHAR(255) NOT NULL, type_source VARCHAR(20) NOT NULL, last_series INT NOT NULL, description LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, is_deleted TINYINT(1) DEFAULT 0 NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE serials DROP last_series');
+        $this->addSql('DROP TABLE serials');
     }
 }
